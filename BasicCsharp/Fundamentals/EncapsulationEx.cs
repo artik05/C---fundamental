df3f66c22c32248ebc5fadeb637a24fdd89d9cec
ramesh-kumar <ramesh.kumar@realpage.com>
2018-01-14 10:15:59 -0600

    c# basic code

---
+++ BasicCsharp/Fundamentals/EncapsulationEx.cs
@@ -0,0 +1,44 @@
+﻿// import statements for external library
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace - package
+
+namespace Fundamentals
+{
+    public class Vehicle
+    {
+        private int NoOfWheels { get; set; }  // int 32bit
+        private string Model { get; set; }   // string 16bit
+        private string Year { get; set; }
+
+        // default constructor - a constructor that takes 0 arguments
+        // provided by compiler if their is no other constructor defined
+        //public Vehicle()
+        //{
+
+        //}
+
+        // if we remove defaul constructor above will get belwo errors.
+       //'Fundamentals.Vehicle' does not contain a constructor that takes 0 arguments
+
+       
+        // I want to call custom constructor with parameters with their types.
+        public Vehicle(int noOfWheel, string model, string year)
+        {
+            this.Model = model;
+        }
+
+        // so private members or methods can only be accessed with in class.
+        public void Display(string model)
+        {
+            this.Model = model;
+
+            // display vehcile information
+            System.Console.Write("Vehicle model - {0}", this.Model);
+        }
+    }
+}
---
+++ BasicCsharp/Fundamentals/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           //Vehicle v = new Vehicle();  //'Fundamentals.Vehicle' does not contain a constructor that takes 0 arguments
+
+           // Error	2	A local variable named 'v' is already defined in this scope	
+
+           Vehicle v = new Vehicle(4,"i10","2010");  // object created and memory allocated for 3 variables
+
+
+            // display information by calling display method
+            //Error	1	'Fundamentals.Vehicle.Display()' is inaccessible due to its protection level means method is private not public
+
+            v.Display("i20");
+            //v.NoOfWheels = 2;
+
+            System.Console.Read();
+        }
+    }
+}
